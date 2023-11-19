89c6b84eb782b2e2f4b1f00a418d444600c8a2b0
milousuga <113415503+milousuga@users.noreply.github.com>
2023-11-19 05:55:57 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,5 @@
+http.createServer(function (req, res) {
+    var index = fs.readFileSync('index.html', 'utf8');
+    res.writeHead(200, {'Content-Type': 'html'});
+    res.end(index);
+  }).listen(3000);
